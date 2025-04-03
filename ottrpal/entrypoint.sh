9e954ae4fc7da65cae4a51097f2c21d96288117d
Candace Savonen <cansav09@gmail.com>
2025-04-03 15:27:57 -0400

    Update ottrpal image

--- ottrpal/entrypoint.sh
+++ ottrpal/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-Rscript -e "ottrpal::ottr_check(check_type = '$1', path = '$2')"
-
-# Save output
-echo "error_name=$error_name" >> $GITHUB_OUTPUT
-echo "report_path=$report_path" >> $GITHUB_OUTPUT
-echo "chk_results=$chk_results" >> $GITHUB_OUTPUT
+if [ "$1" = "rmd" ]; then
+  Rscript -e "bookdown::render_book('index.Rmd', output_format = 'all')"
+elif [ "$1" = "quarto" ]; then
+  Rscript -e "quarto::quarto_render('.')"
+elif [ "$1" = "spelling" ] || [ "$1" = "urls" ] || [ "$1" = "quiz_format" ]; then
+  Rscript -e "ottrpal::ottr_check(check_type = '$1')"
+  # Save output
+  echo "error_name=$error_name" >> $GITHUB_OUTPUT
+  echo "report_path=$report_path" >> $GITHUB_OUTPUT
+  echo "chk_results=$chk_results" >> $GITHUB_OUTPUT
+else
+ echo Viable options are "render_rmd", "render_quarto", "spelling", "urls", "quiz_format"
+fi
