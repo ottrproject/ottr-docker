39b7dacd0445f23bff11b08ebfeb7eb33d7fa51f
Candace Savonen <cansav09@gmail.com>
2025-04-04 14:12:55 -0400

    Update ottrpal

--- ottrpal/entrypoint.sh
+++ ottrpal/entrypoint.sh
@@ -2,14 +2,19 @@
 
 if [ "$1" = "rmd" ]; then
   Rscript -e "bookdown::render_book('index.Rmd', output_format = 'all')"
-elif [ "$1" = "quarto" ]; then
+elif [ "$1" = "quarto" ] || [ "$1" = "quarto_web" ]; then
   Rscript -e "quarto::quarto_render('.')"
+elif [ "$1" = "rmd_web" ]; then
+  Rscript -e "rmarkdown::render_site()"
 elif [ "$1" = "spelling" ] || [ "$1" = "urls" ] || [ "$1" = "quiz_format" ]; then
   Rscript -e "ottrpal::ottr_check(check_type = '$1')"
-  # Save output
-  echo "error_name=$error_name" >> $GITHUB_OUTPUT
-  echo "report_path=$report_path" >> $GITHUB_OUTPUT
-  echo "chk_results=$chk_results" >> $GITHUB_OUTPUT
+
+  # Save output if we are on github
+  if [ -n "$GITHUB_REPOSITORY" ]; then
+    echo "error_name=$error_name" >> $GITHUB_OUTPUT 2>/dev/null
+    echo "report_path=$report_path" >> $GITHUB_OUTPUT 2>/dev/null
+    echo "chk_results=$chk_results" >> $GITHUB_OUTPUT 2>/dev/null
+  fi
 else
- echo Viable options are "render_rmd", "render_quarto", "spelling", "urls", "quiz_format"
+ echo Error: Viable options are "render_rmd", "render_quarto", "spelling", "urls", "quiz_format"
 fi
